77ac1cbd341ea8dc21a3ffded1320e810f390e01
Ivanildo Junior <ivanildo@outlook.com>
2023-03-19 15:00:02 -0300

    feat. added new rote / login template

--- src/main/java/br/com/controller/HomeController.java
+++ src/main/java/br/com/controller/HomeController.java
@@ -1,10 +1,7 @@
 package br.com.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
@@ -12,4 +9,10 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
+
 }
